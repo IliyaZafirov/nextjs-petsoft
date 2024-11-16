5e0e7e2789e1a62ee1726ed222fa2b2cdb6ec44e
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-11-16 05:40:01 +0200

    update selected Pet Id

--- src/contexts/pet-context-provider.tsx
+++ src/contexts/pet-context-provider.tsx
@@ -4,28 +4,35 @@ import { Pet } from "@/lib/types";
 import { createContext, useState } from "react";
 
 type PetContextProviderProps = {
-  data: Pet[]
-  children: React.ReactNode
-}
+  data: Pet[];
+  children: React.ReactNode;
+};
 
 type TPetConext = {
-   pets: Pet[];
-   selectedPetId: string | null;
-}
+  pets: Pet[];
+  selectedPetId: string | null;
+  handleChangeSelectedPetId: (id: string) => void;
+};
 
-export const PetContext = createContext<TPetConext | null >(null);
+export const PetContext = createContext<TPetConext | null>(null);
 
 export default function PetContextProvider({
-  data, children,
+  data,
+  children,
 }: PetContextProviderProps) {
   const [pets, setPets] = useState(data);
-  const [selectedPetId, setSelectedPetId] = useState(null);
+  const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
+
+  const handleChangeSelectedPetId = (id: string) => {
+    setSelectedPetId(id);
+  }
 
   return (
     <PetContext.Provider
       value={{
         pets,
         selectedPetId,
+        handleChangeSelectedPetId,
       }}
     >
       {children}
